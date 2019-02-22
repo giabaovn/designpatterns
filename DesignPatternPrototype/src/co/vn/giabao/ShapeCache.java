745c7dd0cb71f836ad70c9666782359d96e38418
Gia Bao TSC <contact@giabao.vn>
2019-02-22 17:46:45 +0700

    Pattern Group: Creational Pattern Pattern Name: Prototype Design Pattern Concrete Demo: Shape

---
+++ DesignPatternPrototype/src/co/vn/giabao/ShapeCache.java
@@ -0,0 +1,33 @@
+package co.vn.giabao;
+
+import java.util.Hashtable;
+
+public class ShapeCache {
+
+	private static Hashtable<String, Shape> shapeMap = new Hashtable<>();
+	
+	public static Shape getShape(String shapeId) {
+		
+		Shape cacheShape = shapeMap.get(shapeId);
+		
+		return (Shape) cacheShape.clone();
+	}
+
+	// for each Shape run database query and create shape
+	// shapeMap.put(shapeKey, shape)
+	// for example, we are adding three shapes
+	public static void loadCache() {
+		
+		Circle circle = new Circle();
+		circle.setId("1");
+		shapeMap.put(circle.getId(), circle);
+		
+		Square square = new Square();
+		square.setId("2");
+		shapeMap.put(square.getId(), square);
+		
+		Rectangle rectangle = new Rectangle();
+		rectangle.setId("3");
+		shapeMap.put(rectangle.getId(), rectangle);
+	}
+}
